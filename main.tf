0a75c2afd47aed71e2c72334985280e1e503b2fa
sophia3yang <45501509+sophia3yang@users.noreply.github.com>
2023-02-10 17:07:28 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  instance_stat = "running"
 
   tags = {
     Name = "HelloWorld"
