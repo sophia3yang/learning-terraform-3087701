91ac66ef81ac33e10814ecac3a4aa469448ec09b
sophia3yang <45501509+sophia3yang@users.noreply.github.com>
2023-02-14 15:56:29 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2"]
+  azs             = ["us-west-2a"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
