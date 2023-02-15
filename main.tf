5f2e6022db753a17725aced51065e444049f1795
sophia3yang <45501509+sophia3yang@users.noreply.github.com>
2023-02-14 16:06:06 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
   
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.vpc.public_subnets.0
   name ="blog_new"
   
   ingress_rules        = ["https-443-tcp"]
