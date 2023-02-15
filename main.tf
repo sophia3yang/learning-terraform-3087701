d47d62158731124ae253873faa5a8349f9518dfc
sophia3yang <45501509+sophia3yang@users.noreply.github.com>
2023-02-14 16:26:58 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,12 +17,12 @@ data "aws_ami" "app_ami" {
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
-
+  version = "3.19.0"
   name = "dev"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["10.0.111.0/24", "10.0.112.0/24", "10.0.113.0/24"]
 
   enable_nat_gateway = true
 
@@ -44,7 +44,7 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.1"
+  version = "4.17.1"
   name ="blog_new"
   
   vpc_id = module.vpc.public_subnets[0]
