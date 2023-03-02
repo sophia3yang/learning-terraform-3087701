325cfcd84c818ec3d6deadbd880cd3e0db3b4b9a
sophia3yang <45501509+sophia3yang@users.noreply.github.com>
2023-03-01 18:19:30 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ module "alb" {
 
   vpc_id             = module.vpc.vpc_id
   subnets            = module.vpc.public_subnets
-  security_groups    = module.blog_sg.security_group_id
+  security_groups    = module.blog_sg
 
   access_logs = {
     bucket = "dev-alb-logs"
