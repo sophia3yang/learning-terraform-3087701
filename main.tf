eae46db0d00f79e27d65e37b4a800e4a0e07623d
sophia3yang <45501509+sophia3yang@users.noreply.github.com>
2023-03-01 18:04:29 -0800

    Update main.tf
    added ALB

--- main.tf
+++ main.tf
@@ -57,6 +57,55 @@ module "blog_sg" {
   egress_cidr_blocks   = ["0.0.0.0/0"] 
 }
   
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.vpc.vpc_id
+  subnets            = module.vpc.public_subnets
+  security_groups    = module.blog_sg.security_groups.id
+
+  access_logs = {
+    bucket = "dev-alb-logs"
+  }
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+        
+      }
+    }
+  ]
+
+  
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+  
+   tags = 
+   {
+     Environment = "dev"
+   }
+   
+
+}
   
 
 
