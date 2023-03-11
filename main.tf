b4473ef024551d30da0469b868a5dfd6854d8a7d
sophia3yang <45501509+sophia3yang@users.noreply.github.com>
2023-03-10 16:49:06 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ module "blog_vpc" {
   cidr = "${var.vpc_environment.cidr_prefix}.0.0/16"
 
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
-  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
+  public_subnets  = ["${var.vpc_environment.cidr_prefix}.101.0/24", "${var.vpc_environment.cidr_prefix}.102.0/24", "${var.vpc_environment.cidr_prefix}.103.0/24"]
 
   enable_nat_gateway = true
 
